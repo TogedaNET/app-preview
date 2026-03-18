9642ef1c2794ccd0663f708aa49c09494914ab5a
Kaloyan Mitev <kaloyan@de-centra.com>
2026-03-18 21:49:35 +0200

    removed assetlinks

--- next.config.js
+++ next.config.js
@@ -12,10 +12,6 @@ const config = {
         source: "/.well-known/apple-app-site-association",
         destination: "https://api.togeda.net/.well-known/apple-app-site-association",
       },
-      {
-        source: "/.well-known/assetlinks.json",
-        destination: "https://api.togeda.net/.well-known/assetlinks.json",
-      },
     ];
   },
 };
